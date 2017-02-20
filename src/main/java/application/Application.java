188ae27980db7c009e9c82ab20f1a48f48ad51e8
Jan <jan.merta.90@gmail.com>
2017-02-20 21:21:22 +0100

    comment unuseful code

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -32,35 +32,35 @@ public class Application {
         return (args) -> {
             Address a1 = new Address("Ad", 1, "a", "a", "a");
             addressRepository.save(a1);
-            // save a couple of customers
-            customerRepository.save(new application.persistence.entity.Customer("Jack", "Bauer", a1));
+            customerRepository.save(new Customer("Jack", "Bauer", a1));
+
 //            customerRepository.save(new application.persistence.entity.Customer("Chloe", "O'Brian", a1));
 //            customerRepository.save(new application.persistence.entity.Customer("Kim", "Bauer", a1));
 //            customerRepository.save(new application.persistence.entity.Customer("David", "Palmer", a1));
 //            customerRepository.save(new application.persistence.entity.Customer("Michelle", "Dessler", a1));
 
-            // fetch all customers
-            log.info("Customers found with findAll():");
-            log.info("-------------------------------");
-            for (application.persistence.entity.Customer customer : customerRepository.findAll()) {
-                log.info(customer.toString());
-            }
-            log.info("");
-
-            // fetch an individual customer by ID
-            application.persistence.entity.Customer customer = customerRepository.findOne(1L);
-            log.info("Customer found with findOne(1L):");
-            log.info("--------------------------------");
-            log.info(customer.toString());
-            log.info("");
-
-            // fetch customers by last name
-            log.info("Customer found with findByLastName('Bauer'):");
-            log.info("--------------------------------------------");
-            for (application.persistence.entity.Customer bauer : customerRepository.findByLastName("Bauer")) {
-                log.info(bauer.toString());
-            }
-            log.info("");
+//            // fetch all customers
+//            log.info("Customers found with findAll():");
+//            log.info("-------------------------------");
+//            for (application.persistence.entity.Customer customer : customerRepository.findAll()) {
+//                log.info(customer.toString());
+//            }
+//            log.info("");
+//
+//            // fetch an individual customer by ID
+//            application.persistence.entity.Customer customer = customerRepository.findOne(1L);
+//            log.info("Customer found with findOne(1L):");
+//            log.info("--------------------------------");
+//            log.info(customer.toString());
+//            log.info("");
+//
+//            // fetch customers by last name
+//            log.info("Customer found with findByLastName('Bauer'):");
+//            log.info("--------------------------------------------");
+//            for (application.persistence.entity.Customer bauer : customerRepository.findByLastName("Bauer")) {
+//                log.info(bauer.toString());
+//            }
+//            log.info("");
         };
     }
 
