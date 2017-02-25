b30d2ee341c28f7612dae66be9ed0fb158695df5
Jan <jan.merta.90@gmail.com>
2017-02-25 18:14:55 +0100

    reformated code in customer entity

--- src/main/java/application/persistence/entity/Customer.java
+++ src/main/java/application/persistence/entity/Customer.java
@@ -17,38 +17,36 @@ public class Customer implements DTOConvertable<CustomerDTO>, Serializable {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+
     private String firstName;
+
     private String lastName;
+
     @Enumerated(EnumType.STRING)
     private Sex sex;
+
     @OneToOne
     private Address billingAddress;
+
     @OneToOne
     private Address deliveryAddress;
+
     private String phoneCode;
+
     private String phoneNumber;
+
     private String abraLink;
+
     @Enumerated(EnumType.STRING)
     private State state;
+
     @Enumerated(EnumType.STRING)
     private Currency currency;
+
     @Enumerated(EnumType.STRING)
     private Locale locale;
 
     public Customer() {}
-
-    public Customer(String firstName, String lastName, Address deliveryAddress) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.deliveryAddress = deliveryAddress;
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "CustomerDTO[id=%d, firstName='%s', lastName='%s']",
-                id, firstName, lastName);
-    }
 
     @Override
     public CustomerDTO toDTO() {
