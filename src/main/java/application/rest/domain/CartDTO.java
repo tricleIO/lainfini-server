3584395d69afc2ed5fee346b5b05a085dd8c13b5
Jan <jan.merta.90@gmail.com>
2017-02-26 23:35:45 +0100

    added hateoas to cart and its products

--- src/main/java/application/rest/domain/CartDTO.java
+++ src/main/java/application/rest/domain/CartDTO.java
@@ -1,14 +1,20 @@
 package application.rest.domain;
 
 import application.persistence.entity.Cart;
+import application.rest.CartController;
+import application.rest.CustomerController;
 import lombok.Data;
+import org.springframework.hateoas.ResourceSupport;
 
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 @Data
-public class CartDTO implements ReadWriteDatabaseDTO<Cart> {
+public class CartDTO extends ResourceSupport implements ReadWriteDatabaseDTO<Cart> {
 
     private Long uid;
     private Long ownerId;
@@ -26,4 +32,14 @@ public class CartDTO implements ReadWriteDatabaseDTO<Cart> {
         products.add(itemDTO);
     }
 
+    @Override
+    public void addLinks() {
+        add(linkTo(methodOn(CartController.class).readCart(uid)).withSelfRel());
+        if (ownerId != null) {
+            add(linkTo(methodOn(CustomerController.class).readCustomer(ownerId)).withRel("owner"));
+        }
+        for (ItemDTO item : products) {
+            item.addLinks();
+        }
+    }
 }
--- src/main/java/application/rest/domain/ItemDTO.java
+++ src/main/java/application/rest/domain/ItemDTO.java
@@ -1,11 +1,22 @@
 package application.rest.domain;
 
+import application.rest.ProductController;
 import lombok.Data;
+import org.springframework.hateoas.ResourceSupport;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @Data
-public class ItemDTO {
+public class ItemDTO extends ResourceSupport implements Linkable {
 
     private Long productUid;
     private Integer number;
 
+    @Override
+    public void addLinks() {
+        if (productUid != null) {
+            add(linkTo(methodOn(ProductController.class).readProduct(productUid)).withRel("product"));
+        }
+    }
 }
