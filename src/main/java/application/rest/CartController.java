38e0bbe7ae2981aae1b82f58d9f66d2adf5967f6
Jan <jan.merta.90@gmail.com>
2017-03-17 13:23:18 +0100

    removed old route

--- src/main/java/application/rest/CartController.java
+++ src/main/java/application/rest/CartController.java
@@ -2,10 +2,7 @@ package application.rest;
 
 import application.persistence.entity.Cart;
 import application.rest.domain.CartDTO;
-import application.rest.domain.CartItemDTO;
 import application.service.cart.CartService;
-import application.service.cartItem.CartItemService;
-import application.service.response.ServiceResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -20,9 +17,6 @@ public class CartController extends AbstractDatabaseController<Cart, UUID, CartD
     @Autowired
     private CartService cartService;
 
-    @Autowired
-    private CartItemService cartItemService;
-
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> readCarts(Pageable pageable) {
         return readAll(pageable);
@@ -36,16 +30,6 @@ public class CartController extends AbstractDatabaseController<Cart, UUID, CartD
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<?> createCart(@RequestBody CartDTO cart) {
         return create(cart);
-    }
-
-    @RequestMapping(value = "/{cartId}", method = RequestMethod.PATCH)
-    public ResponseEntity<?> addProduct(@PathVariable UUID cartId, @RequestBody CartItemDTO itemDTO) {
-        itemDTO.setCartUid(cartId);
-        ServiceResponse<CartItemDTO> cartItemResponse = cartItemService.create(itemDTO);
-        if (!cartItemResponse.isSuccessful()) {
-            return new ErrorResponseEntity(cartItemResponse.getStatus());
-        }
-        return read(cartId);
     }
 
     @Override
