731bcffc1e42ce02347989133f41a1ad9a9dc3d7
Jan <jan.merta.90@gmail.com>
2017-03-24 20:36:59 +0100

    #33 added slug generating and checking to material

--- src/main/java/application/service/material/MaterialServiceImpl.java
+++ src/main/java/application/service/material/MaterialServiceImpl.java
@@ -4,6 +4,9 @@ import application.persistence.entity.Material;
 import application.persistence.repository.MaterialRepository;
 import application.rest.domain.MaterialDTO;
 import application.service.BaseDatabaseServiceImpl;
+import application.service.response.ServiceResponse;
+import application.service.response.ServiceResponseStatus;
+import application.util.SlugMaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,21 @@ public class MaterialServiceImpl extends BaseDatabaseServiceImpl<Material, Integ
 
     @Autowired
     private MaterialRepository materialRepository;
+
+    @Override
+    protected ServiceResponse<MaterialDTO> doBeforeConvertInCreate(MaterialDTO materialDTO) {
+        // if url slug is null, generate it from name
+        if (materialDTO.getSlug() != null) {
+            if (materialRepository.countBySlug(materialDTO.getSlug()) > 0) {
+                return ServiceResponse.error(ServiceResponseStatus.SLUG_ALREADY_EXISTS);
+            }
+        } else {
+            materialDTO.setSlug(
+                    SlugMaker.getSlugFromString(materialDTO.getName(), materialRepository)
+            );
+        }
+        return super.doBeforeConvertInCreate(materialDTO);
+    }
 
     @Override
     public MaterialRepository getRepository() {
