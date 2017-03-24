58ff6f45f3e0d8cff2c864f7ec1b259602f75cd4
Jan <jan.merta.90@gmail.com>
2017-03-24 23:25:32 +0100

    #33 slugmaker moved to slugservice interface

--- src/main/java/application/persistence/SlugService.java
+++ src/main/java/application/persistence/SlugService.java
@@ -6,13 +6,11 @@ import application.rest.domain.ReadWriteDatabaseDTO;
 import application.rest.domain.SlugDTO;
 import application.service.response.ServiceResponse;
 import application.service.response.ServiceResponseStatus;
-import application.util.SlugMaker;
 
 public interface SlugService<E extends SlugEntity & DTOConvertable<D>, R extends SlugRepository<E>, D extends SlugDTO & ReadWriteDatabaseDTO<E>> {
 
     default ServiceResponse<D> generateSlugAndSetItToDTO(D dto) {
         // if url slug is null, generate it from name
-        // @TODO - write some generic code (interface with repository and generic DTO) for this (product, material, this...)
         if (dto.getSlug() != null) {
             if (getRepository().countBySlug(dto.getSlug()) > 0) {
                 return ServiceResponse.error(ServiceResponseStatus.SLUG_ALREADY_EXISTS);
@@ -34,5 +32,38 @@ public interface SlugService<E extends SlugEntity & DTOConvertable<D>, R extends
     }
 
     R getRepository();
+
+    class SlugMaker {
+
+        public static <E extends SlugEntity & DTOConvertable<D>, R extends SlugRepository<E>, D extends SlugDTO & ReadWriteDatabaseDTO<E>>
+        String getSlugFromString(String string, R slugRepository) {
+            // get slug from transformed string to lower case, remove spaces
+            final String slug = string.replaceAll("\\s+", "-").toLowerCase();
+            // same slug is already in database
+            if (slugAlreadyExists(slug, slugRepository)) {
+                // count postfixed slugs
+                final Long countSimilarSlugs = slugRepository.countBySlugStartingWith(slug + "_");
+                // try new slug postfix is dependent on count of previous slug postfix
+                Long slugPostfix = countSimilarSlugs + 1;
+                // if already exists
+                while (slugAlreadyExists(getPostfixedSlug(slug, slugPostfix), slugRepository)) {
+                    // try increasing postfix until it is unique
+                    slugPostfix++;
+                }
+                return getPostfixedSlug(slug, slugPostfix);
+            }
+            return slug;
+        }
+
+        private static <E extends SlugEntity & DTOConvertable<D>, R extends SlugRepository<E>, D extends SlugDTO & ReadWriteDatabaseDTO<E>>
+        boolean slugAlreadyExists(String slug, R slugRepository) {
+            return slugRepository.countBySlug(slug) > 0;
+        }
+
+        private static String getPostfixedSlug(String slug, Long postfix) {
+            return slug + "_" + postfix;
+        }
+
+    }
 
 }
