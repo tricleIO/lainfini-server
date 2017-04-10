77b11463dda60f25c60bef0612aff7065ac8d443
Jan <jan.merta.90@gmail.com>
2017-04-10 15:57:29 +0200

    show all countries with "excludePagination=true" parameter on shipping/countries route

--- src/main/java/application/service/country/CountryService.java
+++ src/main/java/application/service/country/CountryService.java
@@ -1,11 +1,13 @@
 package application.service.country;
 
+import application.persistence.entity.Country;
 import application.rest.domain.CountryDTO;
+import application.service.BaseDatabaseService;
 import application.service.response.ServiceResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public interface CountryService {
+public interface CountryService extends BaseDatabaseService<Country, Integer, CountryDTO> {
 
     ServiceResponse<Page<CountryDTO>> readAll(Pageable pageable);
 
