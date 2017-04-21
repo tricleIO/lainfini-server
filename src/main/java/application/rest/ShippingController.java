5f21afa9de94006363f7c38b7c3961cf28f12890
Jan <jan.merta.90@gmail.com>
2017-04-21 17:13:10 +0200

    #162 - shipping rights

--- src/main/java/application/rest/ShippingController.java
+++ src/main/java/application/rest/ShippingController.java
@@ -1,8 +1,11 @@
 package application.rest;
 
 import application.persistence.entity.Delivery;
+import application.persistence.type.UserRoleEnum;
 import application.rest.domain.ShippingDTO;
 import application.service.delivery.DeliveryService;
+import application.service.response.ServiceResponse;
+import application.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -15,18 +18,33 @@ public class ShippingController extends AbstractDatabaseController<Delivery, Lon
     @Autowired
     private DeliveryService deliveryService;
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<?> readMaterials(Pageable pageable) {
+    public ResponseEntity<?> readShipping(Pageable pageable) {
+        ServiceResponse<Boolean> hasRolesResponse = userService.hasCurrentUserDemandedRoles(
+                UserRoleEnum.ROLE_ADMIN
+        );
+        if (!hasRolesResponse.isSuccessful()) {
+            return new ErrorResponseEntity(hasRolesResponse.getStatus());
+        }
         return readAll(pageable);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> readMaterial(@PathVariable Long id) {
+    public ResponseEntity<?> readShipping(@PathVariable Long id) {
         return read(id);
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<?> createMaterial(@RequestBody ShippingDTO shippingDTO) {
+    public ResponseEntity<?> createShipping(@RequestBody ShippingDTO shippingDTO) {
+        ServiceResponse<Boolean> hasRolesResponse = userService.hasCurrentUserDemandedRoles(
+                UserRoleEnum.ROLE_ADMIN
+        );
+        if (!hasRolesResponse.isSuccessful()) {
+            return new ErrorResponseEntity(hasRolesResponse.getStatus());
+        }
         return create(shippingDTO);
     }
 
