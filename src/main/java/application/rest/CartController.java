41cd3a62618716d2fe20c13847dd006ffa22a2a0
Jan <jan.merta.90@gmail.com>
2017-04-21 18:23:55 +0200

    #162 - cart (not items) rights

--- src/main/java/application/rest/CartController.java
+++ src/main/java/application/rest/CartController.java
@@ -1,8 +1,12 @@
 package application.rest;
 
 import application.persistence.entity.Cart;
+import application.persistence.type.UserRoleEnum;
 import application.rest.domain.CartDTO;
 import application.service.cart.CartService;
+import application.service.response.ServiceResponse;
+import application.service.response.ServiceResponseStatus;
+import application.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -17,14 +21,44 @@ public class CartController extends AbstractDatabaseController<Cart, UUID, CartD
     @Autowired
     private CartService cartService;
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> readCarts(Pageable pageable) {
+        ServiceResponse<Boolean> hasRolesResponse = userService.hasCurrentUserDemandedRoles(
+                UserRoleEnum.ROLE_ADMIN
+        );
+        if (!hasRolesResponse.isSuccessful()) {
+            return new ErrorResponseEntity(hasRolesResponse.getStatus());
+        }
         return readAll(pageable);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> readCart(@PathVariable UUID id) {
-        return read(id);
+        ServiceResponse<CartDTO> cartResponse = cartService.read(id);
+        if (cartResponse.isSuccessful()) {
+            CartDTO cartDTO = cartResponse.getBody();
+            // is admin
+            ServiceResponse<Boolean> hasRolesResponse = userService.hasCurrentUserDemandedRoles(
+                    UserRoleEnum.ROLE_ADMIN
+            );
+            if (cartDTO.getCustomerUid() != null) {
+                // or its his
+                ServiceResponse<Boolean> isCurrentUserResponse = userService.isCurrrentUser(
+                        cartDTO.getCustomerUid()
+                );
+                if (!hasRolesResponse.isSuccessful() && !isCurrentUserResponse.isSuccessful()) {
+                    return new ErrorResponseEntity(ServiceResponseStatus.FORBIDDEN);
+                }
+            } else {
+                if (!hasRolesResponse.isSuccessful()) {
+                    return new ErrorResponseEntity(hasRolesResponse.getStatus());
+                }
+            }
+        }
+        return getSimpleResponseEntity(cartResponse);
     }
 
     @RequestMapping(value = "/current", method = RequestMethod.GET)
