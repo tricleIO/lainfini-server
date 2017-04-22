1d019792ec744069ac648a9a6ea9f180560d78ef
Jan <jan.merta.90@gmail.com>
2017-04-22 17:08:14 +0200

    #162 - rights for carts

--- src/main/java/application/rest/CartController.java
+++ src/main/java/application/rest/CartController.java
@@ -5,7 +5,6 @@ import application.persistence.type.UserRoleEnum;
 import application.rest.domain.CartDTO;
 import application.service.cart.CartService;
 import application.service.response.ServiceResponse;
-import application.service.response.ServiceResponseStatus;
 import application.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -37,28 +36,9 @@ public class CartController extends AbstractDatabaseController<Cart, UUID, CartD
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> readCart(@PathVariable UUID id) {
-        ServiceResponse<CartDTO> cartResponse = cartService.read(id);
-        if (cartResponse.isSuccessful()) {
-            CartDTO cartDTO = cartResponse.getBody();
-            // is admin
-            ServiceResponse<Boolean> hasRolesResponse = userService.hasCurrentUserDemandedRoles(
-                    UserRoleEnum.ROLE_ADMIN
-            );
-            if (cartDTO.getCustomerUid() != null) {
-                // or its his
-                ServiceResponse<Boolean> isCurrentUserResponse = userService.isCurrrentUser(
-                        cartDTO.getCustomerUid()
-                );
-                if (!hasRolesResponse.isSuccessful() && !isCurrentUserResponse.isSuccessful()) {
-                    return new ErrorResponseEntity(ServiceResponseStatus.FORBIDDEN);
-                }
-            } else {
-                if (!hasRolesResponse.isSuccessful()) {
-                    return new ErrorResponseEntity(hasRolesResponse.getStatus());
-                }
-            }
-        }
-        return getSimpleResponseEntity(cartResponse);
+        return getSimpleResponseEntity(
+                cartService.readCartSecured(id)
+        );
     }
 
     @RequestMapping(value = "/current", method = RequestMethod.GET)
--- src/main/java/application/rest/CartItemController.java
+++ src/main/java/application/rest/CartItemController.java
@@ -1,8 +1,11 @@
 package application.rest;
 
 import application.persistence.entity.CartItem;
+import application.rest.domain.CartDTO;
 import application.rest.domain.CartItemDTO;
+import application.service.cart.CartService;
 import application.service.cartItem.CartItemService;
+import application.service.response.ServiceResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,19 +16,28 @@ import java.util.UUID;
 @RequestMapping(value = "/carts")
 public class CartItemController extends AbstractDatabaseController<CartItem, Long, CartItemDTO, CartItemService> {
 
-    // @TODO - rights
-
     @Autowired
     private CartItemService cartItemService;
 
+    @Autowired
+    private CartService cartService;
+
     @RequestMapping(value = "/{cartId}/items", method = RequestMethod.POST)
     public ResponseEntity<?> createCartItem(@PathVariable UUID cartId, @RequestBody CartItemDTO cart) {
+        ServiceResponse<CartDTO> cartResponse = cartService.readCartSecured(cartId);
+        if (!cartResponse.isSuccessful()) {
+            return new ErrorResponseEntity(cartResponse.getStatus());
+        }
         cart.setCartUid(cartId);
         return create(cart);
     }
 
     @RequestMapping(value = "/{cartId}/items", method = RequestMethod.PUT)
     public ResponseEntity<?> putCartItem(@PathVariable UUID cartId, @RequestBody CartItemDTO cart) {
+        ServiceResponse<CartDTO> cartResponse = cartService.readCartSecured(cartId);
+        if (!cartResponse.isSuccessful()) {
+            return new ErrorResponseEntity(cartResponse.getStatus());
+        }
         cart.setCartUid(cartId);
         return getSimpleResponseEntity(
                 cartItemService.put(cart)
@@ -34,6 +46,10 @@ public class CartItemController extends AbstractDatabaseController<CartItem, Lon
 
     @RequestMapping(value = "/{cartId}/items/{productId}", method = RequestMethod.DELETE)
     public ResponseEntity<?> deleteCartItem(@PathVariable UUID cartId, @PathVariable UUID productId) {
+        ServiceResponse<CartDTO> cartResponse = cartService.readCartSecured(cartId);
+        if (!cartResponse.isSuccessful()) {
+            return new ErrorResponseEntity(cartResponse.getStatus());
+        }
         return getSimpleResponseEntity(
                 cartItemService.removeProductFromCart(productId, cartId)
         );
@@ -44,4 +60,4 @@ public class CartItemController extends AbstractDatabaseController<CartItem, Lon
         return cartItemService;
     }
 
-}
+};
