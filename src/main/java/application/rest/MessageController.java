038068d7ff7f122316f61ab9235e395238a36478
Jan <jan.merta.90@gmail.com>
2017-04-22 17:42:41 +0200

    #162 - temporary admin rights on messages controller

--- src/main/java/application/rest/MessageController.java
+++ src/main/java/application/rest/MessageController.java
@@ -1,5 +1,6 @@
 package application.rest;
 
+import application.persistence.type.UserRoleEnum;
 import application.rest.domain.MailDTO;
 import application.service.mail.MailService;
 import application.service.mailchimp.MailChimpService;
@@ -7,6 +8,7 @@ import application.service.mailchimp.model.MailChimpSubscribeModel;
 import application.service.nexmo.SMSClientService;
 import application.service.nexmo.model.SMSMessageRestModel;
 import application.service.response.ServiceResponse;
+import application.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,20 +26,40 @@ public class MessageController {
     @Autowired
     private SMSClientService smsClientService;
 
+    private UserService userService;
+
     @RequestMapping(value = "/sendMail", method = RequestMethod.POST)
     public ResponseEntity<?> sendMail(@RequestBody MailDTO mail) {
+        ServiceResponse<Boolean> hasRolesResponse = userService.hasCurrentUserDemandedRoles(
+                UserRoleEnum.ROLE_ADMIN
+        );
+        if (!hasRolesResponse.isSuccessful()) {
+            return new ErrorResponseEntity(hasRolesResponse.getStatus());
+        }
         ServiceResponse<MailDTO> mailResponse = mailService.sendMail(mail);
         return new ResponseEntity<>(mailResponse.getBody(), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/sendSMSMessage", method = RequestMethod.POST)
     public ResponseEntity<?> sendSMSMessage(@RequestBody SMSMessageRestModel smsMessageRestModel) {
+        ServiceResponse<Boolean> hasRolesResponse = userService.hasCurrentUserDemandedRoles(
+                UserRoleEnum.ROLE_ADMIN
+        );
+        if (!hasRolesResponse.isSuccessful()) {
+            return new ErrorResponseEntity(hasRolesResponse.getStatus());
+        }
         smsClientService.sendMessage(smsMessageRestModel.getToPhoneNumber(), smsMessageRestModel.getMessageText());
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @RequestMapping(value = "/addNewsletterSubscriber", method = RequestMethod.POST)
     public ResponseEntity<?> addNewsletterSubscriber(@RequestBody MailChimpSubscribeModel mail) {
+        ServiceResponse<Boolean> hasRolesResponse = userService.hasCurrentUserDemandedRoles(
+                UserRoleEnum.ROLE_ADMIN
+        );
+        if (!hasRolesResponse.isSuccessful()) {
+            return new ErrorResponseEntity(hasRolesResponse.getStatus());
+        }
         try {
             mailChimpService.addMemberToList(mail.getListId(),mail.getMail());
         } catch (Exception e) {
@@ -48,11 +70,23 @@ public class MessageController {
 
     @RequestMapping(value = "/getMailChimpLists", method = RequestMethod.GET)
     public ResponseEntity<?> getMailChimpLists() {
+        ServiceResponse<Boolean> hasRolesResponse = userService.hasCurrentUserDemandedRoles(
+                UserRoleEnum.ROLE_ADMIN
+        );
+        if (!hasRolesResponse.isSuccessful()) {
+            return new ErrorResponseEntity(hasRolesResponse.getStatus());
+        }
         return mailChimpService.getLists();
     }
 
     @RequestMapping(value = "/getMailChimpLists/{listId}", method = RequestMethod.GET)
     public ResponseEntity<?> getMailChimpLists(@PathVariable String listId) {
+        ServiceResponse<Boolean> hasRolesResponse = userService.hasCurrentUserDemandedRoles(
+                UserRoleEnum.ROLE_ADMIN
+        );
+        if (!hasRolesResponse.isSuccessful()) {
+            return new ErrorResponseEntity(hasRolesResponse.getStatus());
+        }
         return mailChimpService.getLists(listId);
     }
 
